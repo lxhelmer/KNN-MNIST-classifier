a47b37d504745155e9746d661cddc618eed4d866
helmer <helmer.lauronen@gmail.com>
2023-01-30 17:57:18 +0200

    main.py init

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import struct
+
+with open(os.path.join(os.path.dirname(__file__),("../../train/train-images.idx3-ubyte")), 'rb') as f:
+    magic, koko = struct.unpack(">II", f.read(8))
+    rivien_maara, sarakkeiden_maara = struct.unpack(">II", f.read(8))
+    data = np.fromfile(f, dtype=np.dtype(np.uint8).newbyteorder('>'))
+    data = data.reshape((koko, rivien_maara, sarakkeiden_maara))
+
+
+for i in range(1, 2):
+    for y in range(0,rivien_maara):
+        print(data[i,y,:])
+
+plt.imshow(data[1,:,:], cmap='gray')
+plt.show()
+
+
+def showinconsole(number):
+    print("")
+    print("".join(["---"*28]))
+    for y in range(0,28):
+        print("\n")
+        for x in range(0,28):
+            if number[y,x] != 0:
+                print(" # ",end="")
+            else:
+                print(" . ",end="")
+
+    
+for i in range(1, 10):
+    showinconsole(data[i,:,:])

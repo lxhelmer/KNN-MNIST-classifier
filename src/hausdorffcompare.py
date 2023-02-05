a0afed8b6e0dbc67e6b6ace209263b746c9529bc
helmer <helmer.lauronen@gmail.com>
2023-02-05 21:52:05 +0200

    hausdorff kaavat

--- src/hausdorffcompare.py
+++ src/hausdorffcompare.py
@@ -1,2 +1,20 @@
-def hd_distance(clasSet, TrainSet):
-    todo
+import math
+
+def m_hausdoff_etaisyys(luokiteltava, harjoitus):
+    return max(d_6(luokiteltava, harjoitus),d_6(harjoitus,luokiteltava))
+
+def etaisyys_a_B(piste_a, joukko_B)
+    minimi = math.inf
+    for piste_b in joukko_B:
+        if abs(piste_a-piste_b) < minimi:
+            minimi abs(piste_a-piste_b)
+    return minimi
+
+def d_6(joukko_A, joukko_B):
+    summa = 0
+    for piste_a in joukko a:
+        summa += etaisyys_a_B(piste_a, joukkoB)
+    
+    return (1/len(joukko_A)) * summa
+        
+

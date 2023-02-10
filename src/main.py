dd8d1adae722349a38a93c34e53275214553228e
helmer <helmer.lauronen@gmail.com>
2023-02-10 17:34:42 +0200

    Korjauksia ja optimointia k-pienimmän valintaan ja esittämiseen. end-to-end toimivuus!

--- src/main.py
+++ src/main.py
@@ -1,27 +1,30 @@
 import os
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import struct
 from data.pointsetmaker import pistejoukko 
 from data.csvKaantaja import kirjoita_tiedostoon, lue_tiedostosta
-with open(os.path.join(os.path.dirname(__file__),("../../test/t10k-images.idx3-ubyte")), 'rb') as kuvat:
-    magic, kuvat_koko = struct.unpack(">II", kuvat.read(8))
-    rivien_maara, sarakkeiden_maara = struct.unpack(">II", kuvat.read(8))
-    kuva_array = np.fromfile(kuvat, dtype=np.dtype(np.uint8).newbyteorder('>'))
-    kuva_array = kuva_array.reshape((kuvat_koko, rivien_maara, sarakkeiden_maara))
+from kPienimmat import k_pienimmat
 
-with open(os.path.join(os.path.dirname(__file__),("../../test/t10k-labels.idx1-ubyte")), 'rb') as nimikkeet:
-    magic, nimikkeet_koko = struct.unpack(">II", nimikkeet.read(8))
-    nimike_array = np.fromfile(nimikkeet, dtype=np.dtype(np.uint8).newbyteorder('>'))
-    nimike_array = nimike_array.reshape((nimikkeet_koko,))
+harjoitus_polku = "../../train/train-"
+testi_polku = "../../test/t10k-"
 
+def lataa_kuvat(polku):
+    with open(os.path.join(os.path.dirname(__file__),(polku +"images.idx3-ubyte")), 'rb') as kuvat:
+        magic, kuvat_koko = struct.unpack(">II", kuvat.read(8))
+        rivien_maara, sarakkeiden_maara = struct.unpack(">II", kuvat.read(8))
+        kuva_array = np.fromfile(kuvat, dtype=np.dtype(np.uint8).newbyteorder('>'))
+        kuva_array = kuva_array.reshape((kuvat_koko, rivien_maara, sarakkeiden_maara))
 
-#for i in range(1, 2):
-#    for y in range(0,rivien_maara):
-#        print(data[i,y,:])
+    return kuva_array
 
-#plt.imshow(data[1,:,:], cmap='gray')
-#plt.show()
+def lataa_nimikkeet(polku):
+    with open(os.path.join(os.path.dirname(__file__),(polku+ "labels.idx1-ubyte")), 'rb') as nimikkeet:
+        magic, nimikkeet_koko = struct.unpack(">II", nimikkeet.read(8))
+        nimike_array = np.fromfile(nimikkeet, dtype=np.dtype(np.uint8).newbyteorder('>'))
+        nimike_array = nimike_array.reshape((nimikkeet_koko,))
+
+    return nimike_array
 
 
 def showinconsole(number):
@@ -35,30 +38,60 @@ def showinconsole(number):
             else:
                 print(" . ",end="")
 
-def piirra_pistejoukko(pistejoukot):
-#    for numero in pistejoukot:
-#        numerostr = "." * (int(numero[0][0]) + int(numero[0][1]))
-#        numerostr = numerosrt + "#"
-#        for piste in range(1,len(numero)):
-#            numerostr = numerostr + "." * (numero[piste][0] - numero[piste-1][0])
-#            numerostr = numerostr + "." * (numero[piste][1] - numero[piste-1][1])
-#            numerostr = numerostr + "#"
-#        numerostr = numerostr + "." * 783-len(numerostr)
-#        for char in range(0,784):
-#            if char%28 == 0:
-#                numerostr.insert(numerostr[char] + "\n")
-        print (pistejoukot)
+def piirra_pistejoukko(pistejoukko):
+    xs = []
+    ys = []
+    for piste in pistejoukko:
+        ys.append(piste[0])
+        xs.append(piste[1])
+    plt.scatter(xs,ys)
+    plt.ylim(28,0)
+    plt.show()
 
+
+###
     
-for i in range(0, 1):
-    showinconsole(kuva_array[i,:,:])
-    print(i)
-    kirjoita_tiedostoon(np.asarray(pistejoukko(kuva_array[i,:,:])),"rajat.csv")
-    kirjoita_tiedostoon([nimike_array[i]],"nimikkeet.csv")
+#for i in range(0,60000):
+#    kuvat = lataa_kuvat(harjoitus_polku)
+#    nimikkeet = lataa_nimikkeet(harjoitus_polku)
+#    showinconsole(kuvat[0,:,:])
+#    print(i)
+#    kirjoita_tiedostoon(pistejoukko(kuvat[i,:,:]),"rajat.csv")
+#    kirjoita_tiedostoon([nimikkeet[i]],"nimikkeet.csv")
 
-piirra_pistejoukko(lue_tiedostosta("rajat.csv"))
+
+
+#piirra_pistejoukko(lue_tiedostosta("rajat.csv")[0])
     
 
+k = 5
+luokiteltava = pistejoukko(lataa_kuvat(testi_polku)[0])
+harjoitusdata = lue_tiedostosta("rajat.csv")
+harjoitusnimikkeet = lue_tiedostosta("nimikkeet.csv")
+testinimikkeet = lataa_nimikkeet(testi_polku)
 
+print(k)
+#print(luokiteltava)
+#print(harjoitusdata)
+#print(harjoitusnimikkeet)
+#piirra_pistejoukko(luokiteltava)
+#arvio = k_pienimmat(k, luokiteltava, harjoitusdata, harjoitusnimikkeet)
+#print(arvio)
 
+tulokset = []
 
+for i in range(0,10):
+    luokiteltava = pistejoukko(lataa_kuvat(testi_polku)[i]) 
+    arvio = k_pienimmat(k, luokiteltava, harjoitusdata, harjoitusnimikkeet)
+    tulokset.append([arvio,testinimikkeet[i]])
+
+    print("-"*20)
+
+    print(i+1)
+
+for res in tulokset:
+    print("Arvioitu luku: " + str(res[0]) + " | Todellinen luku: " + str(res[1]),end="")
+    if res[0] == res[1]:
+        print(" |  SAMA!!!")
+    else:
+        print("")

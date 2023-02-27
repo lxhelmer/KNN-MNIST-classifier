bcc19604c7f1d8826607e9d77bb4822487f9d024
helmer <helmer.lauronen@gmail.com>
2023-02-27 17:57:56 +0200

    optimointia testattuna

--- src/hausdorffvertailu.py
+++ src/hausdorffvertailu.py
@@ -5,8 +5,8 @@ class HausdorffVertailu:
         pass
 
     def m_hausdorff_etaisyys(self, luokiteltava, harjoitus, ruudut_luokiteltava, ruudut_harjoitus):
-        AB = self.d_6(luokiteltava, harjoitus, ruudut_luokiteltava)
-        BA = self.d_6(harjoitus,luokiteltava, ruudut_harjoitus)
+        AB = self.d_6(luokiteltava, harjoitus, ruudut_harjoitus)
+        BA = self.d_6(harjoitus,luokiteltava, ruudut_luokiteltava)
         return max(AB,BA)
 
     def minimi_a_b(self, piste_a, piste_b):
@@ -17,33 +17,55 @@ class HausdorffVertailu:
         aY = piste_a[0]
         aX = piste_a[1]
         
-       # lahimmat = ["0,0", #0
-       #             "-1,0","0,1","1,0","0,-1", #1
-       #             "-1,-1","-1,1","1,1","1,-1", #2
-       #             "0,2","2,0","0,-2","-2,0", #4
-       #             "-2,-1","-2,1","-1,2","1,2","2,1",
 
-       # if ruudut_B[aY][aX] == True:
-       #     return 0
+        if ruudut_B[aY][aX] == True:
+            return 0
 
-       # for xy in [-1,1]:
-       #     if aX + xy <= 27 and aX + xy >= 0:
-       #         if ruudut_B[aX+xy][aY] == True:
-       #             return 1
-       #     if aY + xy <= 27 and aY + xy >= 0:
-       #         if ruudut_B[aX][aY+xy] == True:
-       #             return 1
-       # for x in [-1,1]:
-       #     for y in [-1,1]:
-       #         if piste_a[0]+x <= 27 and piste_a[1]+y <= 27 and piste_a[0]+x >= 0 and piste_a[1] + y >= 0:
-       #             if ruudut_B[piste_a[0]+x][piste_a[1]+y] == True:
-       #                     return juuri
+        for et in [[1,0],[-1,0],[0,1],[0,-1]]:
+            if  aY+et[0] >= 0 and aY+et[0] <= 27 and \
+                aX+et[1] >= 0 and aX+et[1] <= 27:
+                if ruudut_B[aY+et[0]][aX+et[1]] == True:
+                    return 1
+            
+        for et in [[1,1],[1,-1],[-1,1],[-1,-1]]:
+            if  aY+et[0] >= 0 and aY+et[0] <= 27 and \
+                aX+et[1] >= 0 and aX+et[1] <= 27:
+                if ruudut_B[aY+et[0]][aX+et[1]] == True:
+                    return juuri
 
-        for piste_b in joukko_B:
+        for et in [[2,0],[-2,0],[0,2],[0,-2]]:
+            if  aY+et[0] >= 0 and aY+et[0] <= 27 and \
+                aX+et[1] >= 0 and aX+et[1] <= 27:
+                if ruudut_B[aY+et[0]][aX+et[1]] == True:
+                    return 2
+
+        for et in [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]]:
+            if  aY+et[0] >= 0 and aY+et[0] <= 27 and \
+                aX+et[1] >= 0 and aX+et[1] <= 27:
+                if ruudut_B[aY+et[0]][aX+et[1]] == True:
+                    return math.sqrt(5)
+
+        for et in [[2,2],[2,-2],[-2,2],[-2,-2]]:
+            if  aY+et[0] >= 0 and aY+et[0] <= 27 and \
+                aX+et[1] >= 0 and aX+et[1] <= 27:
+                if ruudut_B[aY+et[0]][aX+et[1]] == True:
+                    return math.sqrt(8)
+            
+        for et in [[3,0],[-3,0],[0,3],[0,-3]]:
+            if  aY+et[0] >= 0 and aY+et[0] <= 27 and \
+                aX+et[1] >= 0 and aX+et[1] <= 27:
+                if ruudut_B[aY+et[0]][aX+et[1]] == True:
+                    return 3
+
+        for piste_b in joukko_B:    
             arvo = self.minimi_a_b(piste_a, piste_b)
+            if arvo == 10:
+                return math.sqrt(10)
+
             if arvo < minimi:
                 minimi = arvo
         
+
         return math.sqrt(minimi)
 
     def d_6(self, joukko_A, joukko_B,ruudut_B):

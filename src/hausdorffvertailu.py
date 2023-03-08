b53adee367bdf1af73fe87b74238cb5f72641e30
helmer <helmer.lauronen@gmail.com>
2023-03-08 17:20:58 +0200

    k hiomista

--- src/hausdorffvertailu.py
+++ src/hausdorffvertailu.py
@@ -2,95 +2,87 @@ import math
 import numpy as np
 from scipy.spatial.distance import cdist
 
+# pylint: disable=C0103
+
 class HausdorffVertailu:
     def __init__(self):
         pass
 
     def m_hausdorff_etaisyys(self, luokiteltava, harjoitus, ruudut_luokiteltava, ruudut_harjoitus):
-        AB = self.d_6(luokiteltava, harjoitus, ruudut_harjoitus)
-        BA = self.d_6(harjoitus,luokiteltava, ruudut_luokiteltava)
-        return max(AB,BA)
+        luokiteltava_harjoitus = self.d_6(luokiteltava, harjoitus, ruudut_harjoitus)
+        harjoitus_luokiteltava = self.d_6(harjoitus,luokiteltava, ruudut_luokiteltava)
+        return max(luokiteltava_harjoitus ,harjoitus_luokiteltava)      #vertaillaan molempiin suuntiin
 
-    def minimi_a_b(self, piste_a, piste_b):
-        return (piste_a[0]-piste_b[0])**2 + (piste_a[1]-piste_b[1])**2
 
     def etaisyys_a_B(self, piste_a, joukko_B, ruudut_B, juuri):
         minimi = 1600
         aY = piste_a[0]
         aX = piste_a[1]
-        
+
         if ruudut_B[aY][aX] == True:
             return 0
-        
-#        etaisyydet = [
-#                [1,0],[-1,0],[0,1],[0,-1], #etäisyys 1
-#                [1,1],[1,-1],[-1,1],[-1,-1],    #etäisyys sqrt 2
-#                [2,0],[-2,0],[0,2],[0,-2],  #etäisyys 2
-#                [2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2],    #etäisyys sqrt 5
-#                [2,2],[2,-2],[-2,2],[-2,-2],    #etäisyys 3
-#                [3,0],[-3,0],[0,3],[0,-3]   #etäisyys sqrt 10
-#                ]
 
-        for et in [[1,0],[-1,0],[0,1],[0,-1]]:
-            if  aY+et[0] >= 0 and aY+et[0] <= 27 and \
-                aX+et[1] >= 0 and aX+et[1] <= 27:
-                if ruudut_B[aY+et[0]][aX+et[1]] == True:
-                    return 1
-            
-        for et in [[1,1],[1,-1],[-1,1],[-1,-1]]:
-            if  aY+et[0] >= 0 and aY+et[0] <= 27 and \
-                aX+et[1] >= 0 and aX+et[1] <= 27:
-                if ruudut_B[aY+et[0]][aX+et[1]] == True:
-                    return juuri
+        kordinaatit = [
+                [1,0],[-1,0],[0,1],[0,-1], #etäisyys 1
+                [1,1],[1,-1],[-1,1],[-1,-1],    #etäisyys sqrt 2
+                [2,0],[-2,0],[0,2],[0,-2],  #etäisyys 2
+                [2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2],    #etäisyys sqrt 5
+                [2,2],[2,-2],[-2,2],[-2,-2],    #etäisyys sqrt(5)
+                [3,0],[-3,0],[0,3],[0,-3], #etäisyys 3
+                [3,1],[3,-1],[-3,1],[-3,-1],[1,3],[1,-3],[-1,3],[-1,-3], #etäisyys sqrt(10)
+                [3,2],[3,-2],[-3,2],[-3,-2],[2,3],[2,-3],[-2,3],[-2,-3] #etäisyys sqrt(13)
+                ]
+        etaisyydet = {
+                (1,0) : 1,
+                (0,1) : 1,
+                (1,1) : math.sqrt(2),
+                (2,0) : 2,
+                (0,2) : 2,
+                (2,1) : math.sqrt(5),
+                (1,2) : math.sqrt(5),
+                (2,2) : math.sqrt(8),
+                (3,0) : 3,
+                (0,3) : 3,
+                (3,1) : math.sqrt(10),
+                (1,3) : math.sqrt(10),
+                (3,2) : math.sqrt(13),
+                (2,3) : math.sqrt(13)
 
-        for et in [[2,0],[-2,0],[0,2],[0,-2]]:
-            if  aY+et[0] >= 0 and aY+et[0] <= 27 and \
-                aX+et[1] >= 0 and aX+et[1] <= 27:
-                if ruudut_B[aY+et[0]][aX+et[1]] == True:
-                    return 2
+                }
 
-        for et in [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]]:
-            if  aY+et[0] >= 0 and aY+et[0] <= 27 and \
-                aX+et[1] >= 0 and aX+et[1] <= 27:
-                if ruudut_B[aY+et[0]][aX+et[1]] == True:
-                    return math.sqrt(5)
+        for kordinaatti in kordinaatit:
+            if  aY+kordinaatti[0] >= 0 and aY+kordinaatti[0] <= 27 and \
+                aX+kordinaatti[1] >= 0 and aX+kordinaatti[1] <= 27:
+                    if ruudut_B[aY+kordinaatti[0]][aX+kordinaatti[1]] == True:
+                        return etaisyydet[(abs(kordinaatti[0]),abs(kordinaatti[1]))]
 
-        for et in [[2,2],[2,-2],[-2,2],[-2,-2]]:
-            if  aY+et[0] >= 0 and aY+et[0] <= 27 and \
-                aX+et[1] >= 0 and aX+et[1] <= 27:
-                if ruudut_B[aY+et[0]][aX+et[1]] == True:
-                    return math.sqrt(8)
-            
-        for et in [[3,0],[-3,0],[0,3],[0,-3]]:
-            if  aY+et[0] >= 0 and aY+et[0] <= 27 and \
-                aX+et[1] >= 0 and aX+et[1] <= 27:
-                if ruudut_B[aY+et[0]][aX+et[1]] == True:
-                    return 3
-        
 
-        for piste_b in joukko_B:    
+
+        for piste_b in joukko_B:
             arvo = (piste_a[0]-piste_b[0])**2 + (piste_a[1]-piste_b[1])**2
 
-
-            if arvo == 10:
-                return math.sqrt(10)
+            if arvo == 18:
+                return math.sqrt(18)
 
             if arvo < minimi:
                 minimi = arvo
 
-        
+
 
         return math.sqrt(minimi)
-        #return np.min(cdist(np.array([piste_a]), joukko_B))    #nopea vaihtoehto ratkaisu etäisyyden laskemiselle
-    
+        #return np.min(cdist(np.array([piste_a]), joukko_B))
+        #nopea vaihtoehto minimi etäisyyden laskemiselle
+        #tosin tätä käyttäess tulee listojen olla numpy array muodossa
+        #eikä etäisyyksien määrää ollut tehokasta karsio numpy arrayn 
+        #hitaamman lookup toiminnon takia.
+
     def d_6(self, joukko_A, joukko_B,ruudut_B):
         juuri = math.sqrt(2)
         summa = 0
-        
+
         for piste_a in joukko_A:
 
 
             summa += self.etaisyys_a_B(piste_a, joukko_B,ruudut_B, juuri)
-        
-        return summa/len(joukko_A)
 
+        return summa/len(joukko_A)
--- src/lataaja.py
+++ src/lataaja.py
@@ -4,18 +4,18 @@ import numpy as np
 
 
 def lataa_kuvat(polku):
-    with open(os.path.join(os.path.dirname(__file__),(polku +"images.idx3-ubyte")), 'rb') as kuvat:
+    with open(os.path.join(os.path.dirname(__file__),(polku +"images.idx3-ubyte")), 'rb') \
+    as kuvat:
         magic, kuvat_koko = struct.unpack(">II", kuvat.read(8))
         rivien_maara, sarakkeiden_maara = struct.unpack(">II", kuvat.read(8))
         kuva_array = np.fromfile(kuvat, dtype=np.dtype(np.uint8).newbyteorder('>'))
         kuva_array = kuva_array.reshape((kuvat_koko, rivien_maara, sarakkeiden_maara))
-
     return kuva_array
 
 def lataa_nimikkeet(polku):
-    with open(os.path.join(os.path.dirname(__file__),(polku+ "labels.idx1-ubyte")), 'rb') as nimikkeet:
+    with open(os.path.join(os.path.dirname(__file__),(polku+ "labels.idx1-ubyte")), 'rb') \
+    as nimikkeet:
         magic, nimikkeet_koko = struct.unpack(">II", nimikkeet.read(8))
         nimike_array = np.fromfile(nimikkeet, dtype=np.dtype(np.uint8).newbyteorder('>'))
         nimike_array = nimike_array.reshape((nimikkeet_koko,))
-
     return nimike_array
